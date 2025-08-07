07e606985f9bd0408d0272aa2ccc910cd7a3cad9
Jacob Brockett <brockett04@gmail.com>
2025-08-07 16:43:11 -0500

    update docker files to include mysqli extension for php on startup

--- UI/Source_Files/helpers/database_helpers.php
+++ UI/Source_Files/helpers/database_helpers.php
@@ -5,7 +5,7 @@ function db_connect($db): mysqli
     $db_user="recipe_user";
     $db_password="sug@r&spic3";
 
-    $dblink = new mysqli($host, $db_user, $db_password, $db, 3306); // ODBC string
+    $dblink = new mysqli($host, $db_user, $db_password, $db, 3306); // ODBC string; port is 3306 between containers for mysql
 
     return $dblink;
 }

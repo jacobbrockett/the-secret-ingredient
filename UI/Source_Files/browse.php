218b1c46f9e8daea5f686afe40dbd85956446ab8
Jacob Brockett <brockett04@gmail.com>
2025-08-07 16:37:40 -0500

    display recipes from MySQL DB container

--- UI/Source_Files/browse.php
+++ UI/Source_Files/browse.php
@@ -6,5 +6,37 @@
         <div class="panel-body">
             <p>Find a recipe even the pickiest eaters will love!</p>
         </div>
+        <div>
+            <table style="width:100%">
+                <tbody>
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>User First Name</th>
+                    <th>User Last Name</th>
+                </tr>
+                <?php
+                error_reporting(E_ALL);
+                ini_set('display_errors', 1);
+
+                include("./helpers/database_helpers.php");
+                $dblink = db_connect("recipe-db");
+
+                $sql = "select recipes.title as 'Title', recipes.Description as 'Description', users.first_name as 'First_Name', users.last_name as 'Last_Name' from recipes INNER JOIN users ON recipes.user_id=users.id;";
+                $results = $dblink->query($sql) or die("<h2>Something went wrong with $sql<br>" . $dblink->error . "</h2>");
+
+                while ($info = $results->fetch_array(MYSQLI_ASSOC)) {
+                    echo "<tr>";
+                    echo "<td>$info[Title]</td>";
+                    echo "<td>$info[Description]</td>";
+                    echo "<td>$info[First_Name]</td>";
+                    echo "<td>$info[Last_Name]</td>";
+                    echo "</tr>";
+                }
+
+                ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
---
+++ UI/Source_Files/helpers/database_helpers.php
@@ -0,0 +1,12 @@
+<?php
+function db_connect($db): mysqli
+{
+    $host="recipe-site-db"; # name of mysql container; since UI and DB containers on same docker network, hostnames between containers are the container names
+    $db_user="recipe_user";
+    $db_password="sug@r&spic3";
+
+    $dblink = new mysqli($host, $db_user, $db_password, $db, 3306); // ODBC string
+
+    return $dblink;
+}
+?>

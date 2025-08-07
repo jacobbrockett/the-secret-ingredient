a8417177f227351071cd54ad2a5a06fbf822e125
Jacob Brockett <brockett04@gmail.com>
2025-08-07 16:35:52 -0500

    sql statements to test DB

--- DB/Select Examples.sql
+++ DB/Select Examples.sql
@@ -6,5 +6,5 @@ select * from instructions;
 
 select * from ingredients;
 
-select recipes.title, recipes.Description, users.first_name, users.last_name from recipes
+select recipes.title as 'Title', recipes.Description as 'Description', users.first_name as 'First_Name', users.last_name as 'Last_Name' from recipes
 INNER JOIN users ON recipes.user_id=users.id;

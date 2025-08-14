b4b96a941b218b834696fcda308bd4f85811cc8b
Jacob Brockett <brockett04@gmail.com>
2025-08-14 18:16:21 -0500

    added function to display recipes from DB as panels

--- UI/Source_Files/functions.php
+++ UI/Source_Files/functions.php
@@ -2,17 +2,6 @@
 
 use JetBrains\PhpStorm\NoReturn;
 
-function db_connect($db): mysqli
-{
-    $host="localhost";
-    $db_user="webuser";
-    $db_password="l*p1Kan_RKTad8(R";
-
-    $dblink = new mysqli($host, $db_user, $db_password, $db); // ODBC string
-
-    return $dblink;
-}
-
 #[NoReturn] function redirect($uri): void
 { ?>
     <script type="text/javascript">

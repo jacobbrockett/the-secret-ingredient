f8d926d76ee2beee0fb59e1feaf4c7df62ea660e
Jacob Brockett <brockett04@gmail.com>
2025-08-18 09:04:39 -0500

    separated function to fetch recipes from DB and display the results

--- UI/Source_Files/browse.php
+++ UI/Source_Files/browse.php
@@ -4,34 +4,46 @@
         <p>Find a recipe even the pickiest eaters will love!</p>
     </div>
     <?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
+//        error_reporting(E_ALL);
+//        ini_set('display_errors', 1);
 
-    display_recipes();
+        // Fetch recipes from database:
+        $results = fetch_recipes();
+
+        // Display the recipes:
+        if ($results)
+        {
+            while ($db_recipe = $results->fetch_array(MYSQLI_ASSOC))
+            {
+                display_recipe($db_recipe);
+            }
+        }
+
     ?>
 </div>
 
 <?php
+    function fetch_recipes(): bool|mysqli_result
+    {
+        include("./helpers/database_helpers.php");
+        $dblink = db_connect("recipe-db");
 
-function display_recipes(): void
-{
-    include("./helpers/database_helpers.php");
-    $dblink = db_connect("recipe-db");
+        $sql = "select recipes.title as 'Title', recipes.Description as 'Description', users.first_name as 'First_Name', users.last_name as 'Last_Name' from recipes INNER JOIN users ON recipes.user_id=users.id;";
+        $results = $dblink->query($sql) or die("<h2>Something went wrong with $sql<br>" . $dblink->error . "</h2>");
 
-    $sql = "select recipes.title as 'Title', recipes.Description as 'Description', users.first_name as 'First_Name', users.last_name as 'Last_Name' from recipes INNER JOIN users ON recipes.user_id=users.id;";
-    $results = $dblink->query($sql) or die("<h2>Something went wrong with $sql<br>" . $dblink->error . "</h2>");
+        return $results;
+    }
 
-    while ($info = $results->fetch_array(MYSQLI_ASSOC)) {
+    function display_recipe($db_recipe): void
+    {
         echo '<div class="panel panel-default">';
         echo '<div class="panel-heading">
-                <h2>' . $info['Title'] . '</h2>
-                <p><i>By ' . $info['First_Name'] . ' ' . $info['Last_Name'] . '</i></p>
-              </div>';
+                    <h2>' . $db_recipe['Title'] . '</h2>
+                    <p><i>By ' . $db_recipe['First_Name'] . ' ' . $db_recipe['Last_Name'] . '</i></p>
+                  </div>';
         echo '<div class="panel-body">
-                <p>' . $info['Description'] . '</p>
-              </div>';
+                    <p>' . $db_recipe['Description'] . '</p>
+                  </div>';
         echo '</div>';
     }
-}
-
 ?>

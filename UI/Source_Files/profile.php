d0fb8b606e20ade8991efdae1af1556afae74886
Jacob Brockett <brockett04@gmail.com>
2025-08-25 09:12:59 -0500

    profile page placeholders for profile and contact info sections

--- UI/Source_Files/profile.php
+++ UI/Source_Files/profile.php
@@ -1,10 +1,47 @@
 <div class="container">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h2>Profile</h2>
+    <div class="page-header">
+        <h1>Profile</h1>
+        <div class="row">
+            <div class="col-md-4">
+                <img src="assets/images/swole_mordecai.jpg" alt="Image of the Regular Show character Mordecai"
+                     style="width:171px;height:180px;">
+            </div>
+            <div class="col-md-8">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu volutpat velit. Sed ut finibus sapien.
+                    In sed ornare sapien, nec pharetra quam. Pellentesque habitant morbi tristique senectus et netus et
+                    malesuada fames ac turpis egestas. Praesent aliquet felis et libero tincidunt vulputate. Maecenas id
+                    sapien congue, luctus massa dignissim, porttitor nibh. Nunc et lobortis risus, vitae pulvinar risus.
+                    Proin consectetur consectetur sollicitudin. Donec ut tristique ligula, non suscipit odio. Quisque
+                    sagittis ornare vestibulum. Pellentesque elementum nisi vitae lectus mollis aliquam eget pulvinar
+                    sapien. Etiam ut eros scelerisque risus pulvinar sodales. Duis luctus imperdiet risus, vel pretium
+                    diam porta egestas. Sed pellentesque egestas lectus eget semper.</p>
+            </div>
         </div>
-        <div class="panel-body">
-            <p>View / update user information</p>
-        </div>
+
+    </div>
+    <div class="page-header">
+        <h1>Contact Information</h1>
+
+        <h2>Name</h2>
+        <p>Lorem Ipsum</p>
+
+        <h2>Email</h2>
+        <p>lorem@ipsum.com</p>
+
+        <h2>Phone Number</h2>
+        <p>(xxx)xxx-xxxx</p>
+
+        <br>
+        <button type="button" class="btn btn-default btn-lg">Edit</button>
+    </div>
+    <div class="page-header">
+        <h1>Uploaded Recipes</h1>
+        <p>View list of condensed recipes user has uploaded</p>
+        <!--        TODO: add functionality to delete recipes-->
+    </div>
+    <div class="page-header">
+        <h1>Favorite Recipes</h1>
+        <p>View list of condensed recipes user has "saved"</p>
+        <!--        TODO: add functionality to "unsave" recipes-->
     </div>
 </div>
